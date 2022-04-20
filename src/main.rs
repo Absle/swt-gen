23bab6de049e7bd8a733f4cafb33a52883a6094b
Anderson Kagel <anderson.kagel@gmail.com>
2022-04-19 22:07:24 -0400

    Change output file names to use raw seconds rather than hex representations

--- src/main.rs
+++ src/main.rs
@@ -11,14 +11,35 @@ fn main() {
     let time = SystemTime::now()
         .duration_since(SystemTime::UNIX_EPOCH)
         .unwrap();
+
     fs::write(
-        format!("output/{}-{:x}.csv", subsector.name(), time.as_secs()),
+        format!("output/{}-{}.csv", subsector.name(), time.as_secs()),
         csv,
     )
     .unwrap();
     fs::write(
-        format!("output/{}-{:x}.svg", subsector.name(), time.as_secs()),
+        format!("output/{}-{}.svg", subsector.name(), time.as_secs()),
         svg,
     )
     .unwrap();
+
+    // let read_in_csv = fs::read_to_string("output/Sonora-6252ea92.csv").unwrap();
+    // let subsector = astrography::Subsector::from_csv(&read_in_csv).unwrap();
+    // let csv = subsector.to_csv();
+    // let svg = subsector.generate_svg();
+
+    // let time = SystemTime::now()
+    //     .duration_since(SystemTime::UNIX_EPOCH)
+    //     .unwrap();
+
+    // fs::write(
+    //     format!("output/{}-{}.csv", subsector.name(), time.as_secs()),
+    //     csv,
+    // )
+    // .unwrap();
+    // fs::write(
+    //     format!("output/{}-{}.svg", subsector.name(), time.as_secs()),
+    //     svg,
+    // )
+    // .unwrap();
 }
